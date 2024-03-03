82e2cd5a523f5fda75176a08eb120a0872700add
Arne Babenhauserheide <arne_bab@web.de>
2024-03-03 09:46:41 +0100

    make package-lint happy (thanks for the review, riscy!)

--- org-clock-agenda-daytime-mode.el
+++ org-clock-agenda-daytime-mode.el
@@ -1,9 +1,14 @@
-;;; org-clock-agenda-daytime-mode --- display the time clocked today in the modeline -*- lexical-binding: t -*-
+;;; org-clock-agenda-daytime-mode.el --- Display the time clocked today in the modeline  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2024 Arne Babenhauserheide
+
+;; Author: Arne Babenhauserheide <arne_bab@web.de>
 ;; Keywords: org, lisp, clock, time, agenda
 ;; Version: 0.0.1
-;; Requires: org
+;; Package-Requires: ((org "9.6.18") (emacs "26.1"))
+;; URL: https://www.draketo.de/software/emacs-daytime
+
+;; This file is NOT part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -17,7 +22,8 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Commentary:
 
 ;; This is a minor mode that show a modeline entry with the time
@@ -34,19 +40,19 @@
 ;;; Code:
 
 ;; add total task time from plan.org to modeline
-(defvar org-total-time--last-time (float-time))
-(defvar org-total-time--last-res "")
+(defvar org-clock-agenda-daytime--last-time (float-time))
+(defvar org-clock-agenda-daytime--last-res "")
 (defvar org-clock-agenda-daytime-modeline-entry)
 
 (require 'org)
-(defun org-total-time ()
+(defun org-clock-agenda-daytime--org-total-time ()
   "Show the total time clocked for today."
   (interactive)
   ;; cache for a minute
-  (if (> 60 (- (float-time) org-total-time--last-time))
-      org-total-time--last-res
-    (setq org-total-time--last-time (float-time))
-    (setq org-total-time--last-res
+  (if (> 60 (- (float-time) org-clock-agenda-daytime--last-time))
+      org-clock-agenda-daytime--last-res
+    (setq org-clock-agenda-daytime--last-time (float-time))
+    (setq org-clock-agenda-daytime--last-res
             (format " [%s] "
                     (org-duration-from-minutes
                      (apply #'+ (mapcar
@@ -55,14 +61,15 @@
                                       (find-file-noselect current-file)
                                     (org-clock-sum-today)))
                                 org-agenda-files)))))
-      org-total-time--last-res))
+      org-clock-agenda-daytime--last-res))
 
+;;;###autoload
 (define-minor-mode org-clock-agenda-daytime-mode
   "Toggle display of the total time clocked today in the modeline."
   :global t :group 'org-clock
   (or global-mode-string (setq global-mode-string '("")))
   (setq org-clock-agenda-daytime-modeline-entry
-        '(:eval (org-total-time)))
+        '(:eval (org-clock-agenda-daytime--org-total-time)))
   (if org-clock-agenda-daytime-mode
 	    (or (memq org-clock-agenda-daytime-modeline-entry global-mode-string)
 	        (setq global-mode-string
