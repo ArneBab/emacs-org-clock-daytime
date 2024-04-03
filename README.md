c27542f084e97ae988108ce2a0ca26d576f9c0df
Arne Babenhauserheide <arne_bab@web.de>
2024-04-03 09:33:36 +0200

    Change modeline display when reaching target time or maximum time

--- README.md
+++ README.md
@@ -15,3 +15,13 @@ If you clocked 32 minutes in total for today in the LOGBOOK drawer:
 You see that time shown in the modeline:
 
 ![modeline entry](modeline-entry.png)
+
+When you reached `org-clock-agenda-daytime--target-work-time-minutes`
+the entry changes the color to the `org-done` face (customizable):
+
+![modeline entry, target time reached](modeline-entry-target-reached.png)
+
+When you reach `org-clock-agenda-daytime--maximum-work-time-minutes`
+the entry display changes to the `org-mode-line-clock-overrun` face (customizable):
+
+![modeline entry, maximum time reached](modeline-entry-maximum.png)
--- org-clock-agenda-daytime-mode.el
+++ org-clock-agenda-daytime-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Arne Babenhauserheide <arne_bab@web.de>
 ;; Keywords: org, lisp, clock, time, agenda
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Package-Requires: ((org "9.6.18") (emacs "26.1"))
 ;; URL: https://www.draketo.de/software/emacs-daytime
 
@@ -34,6 +34,11 @@
 
 ;; Enable the mode with (org-clock-agenda-daytime-mode 1).
 
+;; When you reach a target work time (default: 8 hours), the time
+;; display turns green, after reaching a maximum work time (default:
+;; 10 hours) it turns red. The values and faces are are customizable
+;; in the `org-clock' group (use M-x customize-group org-clock).
+
 ;; The time calculation caches results for one minute to avoid slowing
 ;; down interaction.
 
@@ -44,6 +49,31 @@
 (defvar org-clock-agenda-daytime--last-res "")
 (defvar org-clock-agenda-daytime-modeline-entry)
 
+(defcustom org-clock-agenda-daytime--target-work-time-minutes (* 60 8)
+  "The daily worktime is marked as enough after this many minutes.
+
+ To specify the minutes more conveniently you can use a simple
+ sexp like `(* 60 8)' for a 40 hour week or a more complex one
+ like `(+ 24 (* 60 7))' for a 37 hour week"
+  :group 'org-clock
+  :type '(natnum sexp))
+(defcustom org-clock-agenda-daytime--maximum-work-time-minutes (* 60 10)
+  "The daily worktime is marked as too long after this many minutes.
+
+You can use a sexp like `(* 60 10)' to specify the minutes more
+ conveniently."
+  :group 'org-clock
+  :type '(natnum sexp))
+(defcustom org-clock-agenda-daytime--target-work-time-reached-face 'org-done
+  "The face to use to mark the modeline entry when the target time is reached."
+  :group 'org-clock
+  :type 'face)
+(defcustom org-clock-agenda-daytime--maximum-work-time-reached-face 'org-mode-line-clock-overrun
+  "The face to use to mark the modeline entry when the maximmum time is reached."
+  :group 'org-clock
+  :type 'face)
+
+
 (require 'org)
 (defun org-clock-agenda-daytime--org-total-time ()
   "Show the total time clocked for today."
@@ -52,16 +82,26 @@
   (if (> 60 (- (float-time) org-clock-agenda-daytime--last-time))
       org-clock-agenda-daytime--last-res
     (setq org-clock-agenda-daytime--last-time (float-time))
-    (setq org-clock-agenda-daytime--last-res
-            (format " [%s] "
-                    (org-duration-from-minutes
-                     (apply #'+ (mapcar
-                                (lambda (current-file)
-                                  (with-current-buffer
-                                      (find-file-noselect current-file)
-                                    (org-clock-sum-today)))
-                                org-agenda-files)))))
-      org-clock-agenda-daytime--last-res))
+    (let* ((daytime-minutes (apply #'+ (mapcar
+                                        (lambda (current-file)
+                                          (with-current-buffer
+                                              (find-file-noselect current-file)
+                                            (org-clock-sum-today)))
+                                        org-agenda-files)))
+           (text (format " [%s] "
+                         (org-duration-from-minutes daytime-minutes))))
+      (setq org-clock-agenda-daytime--last-res
+            (cond
+             ((> org-clock-agenda-daytime--target-work-time-minutes daytime-minutes)
+              text)
+             ((> org-clock-agenda-daytime--maximum-work-time-minutes daytime-minutes)
+              (propertize text
+                          'face
+                          org-clock-agenda-daytime--target-work-time-reached-face))
+             (t
+              (propertize text
+                          'face
+                          org-clock-agenda-daytime--maximum-work-time-reached-face)))))))
 
 ;;;###autoload
 (define-minor-mode org-clock-agenda-daytime-mode
@@ -71,9 +111,9 @@
   (setq org-clock-agenda-daytime-modeline-entry
         '(:eval (org-clock-agenda-daytime--org-total-time)))
   (if org-clock-agenda-daytime-mode
-	    (or (memq org-clock-agenda-daytime-modeline-entry global-mode-string)
-	        (setq global-mode-string
-		          (append global-mode-string (list org-clock-agenda-daytime-modeline-entry))))
+	  (or (memq org-clock-agenda-daytime-modeline-entry global-mode-string)
+	      (setq global-mode-string
+		        (append global-mode-string (list org-clock-agenda-daytime-modeline-entry))))
     (and (memq org-clock-agenda-daytime-modeline-entry global-mode-string)
          (setq global-mode-string
                (remove org-clock-agenda-daytime-modeline-entry global-mode-string)))))
